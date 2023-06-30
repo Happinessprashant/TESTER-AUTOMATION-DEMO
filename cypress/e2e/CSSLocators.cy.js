bf074ab022831e9267b5c9905cd8e4a1e054c652
Happinessprashant <125643307+Happinessprashant@users.noreply.github.com>
2023-06-30 19:13:37 +0530

    (test) add new files

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -12,6 +12,9 @@
     //     cy.get(".lighter").contains("T-Shirts")
     // }
     // )
+
+    // #check
+
     describe('The Home Page', () => {
         it('successfully loads', () => {
           cy.visit('http://localhost:3000/') // change URL to match your dev URL
@@ -20,9 +23,9 @@
           cy.contains("button","Sign").should("contain","Sign Up")
 
 
-          if( cy.contains("button","Sign").should("contain","Sign Up")){
+          // if( cy.contains("button","Sign").should("contain","Sign Up")){
 
-          }
+          // }
         })
       })
 
