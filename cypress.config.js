e8987df9fad5474a4df81856885f9050103f0aab
Happinessprashant <125643307+Happinessprashant@users.noreply.github.com>
2023-07-19 12:39:33 +0530

    get msg to slack

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,26 @@
 const { defineConfig } = require("cypress");
+// import * as dotenv from "dotenv"
+// import  dotenv from "dotenv"
+require('dotenv').config()
+
+// dotenv.config();
+
 
 module.exports = defineConfig({
   projectId: 'sq11n6',
   reporter: 'mochawesome',
     "env": {
-      "shell": "powershell.exe"
+      "shell": "powershell.exe",
+      CYPRESS_RECORD_KEY : process.env.CYPRESS_RECORD_KEY,
+     
     }
   ,
   
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here 
+     
     },
+    type: "module",
   },
 });
